11c7fbf8265a89544cb6bc3224ab7ab88f3cc7bd
davidleblanc <david.alan.leblanc@outlook.com>
2020-06-09 13:47:26 -0400

    unit testing complete!

--- CarTests/CarTests.cs
+++ CarTests/CarTests.cs
@@ -1,14 +1,62 @@
+using System;
+using CarNS;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CarTests
 {
+    [TestClass]
     public class CarTests
     {
         //TODO: add emptyTest so we can configure our runtime environment (remove this test before pushing to your personal GitHub account)
+        [TestMethod]
+        public void EmptyTest()
+        {
+            Assert.AreEqual(10, 10, .001);
+        }
         //TODO: constructor sets gasTankLevel properly
+
+        Car test_car;
+
+        [TestInitialize]
+        public void CreateCarObject()
+        {
+            test_car = new Car("Toyota", "Prius", 10, 50);
+        }
+
+        [TestMethod]
+        public void TestInitialGasTank()
+        {
+            //Car test_car = new Car("Toyota", "Prius", 10, 50);
+            Assert.AreEqual(10, test_car.GasTankLevel, .001);
+        }
         //TODO: gasTankLevel is accurate after driving within tank range
+
+        [TestMethod]
+        public void TestGasTankAfterDriving()
+        {
+            //Car test_car = new Car("Toyota", "Prius", 10, 50
+            test_car.Drive(50);
+            Assert.AreEqual(9, test_car.GasTankLevel, .001);
+        }
+
         //TODO: gasTankLevel is accurate after attempting to drive past tank range
+
+        [TestMethod]
+        public void TestGasTankAfterExceedingTankRange()
+        {
+            //Car test_car = new Car("Toyota", "Prius", 10, 50
+            test_car.Drive(501);
+            Assert.AreEqual(0, test_car.GasTankLevel, .001);
+        }
+
         //TODO: can't have more gas than tank size, expect an exception
 
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestGasOverfillException()
+        {
+            test_car.AddGas(5);
+            Assert.Fail("Shouldn't get here, car cannot have more gas in tank than the size of the tank");
+        }
     }
 }
